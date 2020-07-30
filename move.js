937e12266849f612f7cf8c9dbfba21c26f5d7fb9
Charles Ancheta <cancheta@ualberta.ca>
2020-07-30 01:43:41 -0600

    :clown_face: writing to json

--- move.js
+++ move.js
@@ -1,1 +1,17 @@
-console.log("Hi");
+const jsonfile = require("jsonfile");
+const file = "./board.json";
+
+function makeMove(board) {
+  board.left[1] -= 1;
+  return board;
+}
+
+jsonfile.readFile(file, (err, obj) => {
+  if (err) console.error(err);
+
+  const res = makeMove(obj);
+
+  jsonfile.writeFile(file, res, { spaces: 2 }, err => {
+    if (err) console.error(err);
+  });
+});
