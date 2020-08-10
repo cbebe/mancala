74f36fb9381fd283b25e84b1a9ed2a0a89fd98c3
Charles Ancheta <cancheta@ualberta.ca>
2020-08-10 17:44:05 -0600

    :art: change arrow functions to real functions

--- src/update.ts
+++ src/update.ts
@@ -7,37 +7,37 @@ import {
   GameRecord,
 } from "./interfaces";
 
-export const updatePlayers = (players: Players, currentPlayer: string) => {
+export function updatePlayers(players: Players, currentPlayer: string) {
   if (!players[currentPlayer]) players[currentPlayer] = 1;
   else ++players[currentPlayer];
   return players;
-};
+}
 
-export const updateMostRecentMoves = (
+export function updateMostRecentMoves(
   moveArray: MoveObject[],
   moveObj: MoveObject
-) => {
+) {
   moveArray.unshift(moveObj);
   return moveArray.slice(0, 3);
-};
+}
 
-export const updateAfterTurn = (
+export function updateAfterTurn(
   data: Data,
   currentPlayer: string,
   side: Side,
   idx: number
-) => {
+) {
   data.players = updatePlayers(data.players, currentPlayer);
-  ++data.games.totalMoves;
+  ++data.totalMoves;
   const move = { name: currentPlayer, side, idx };
   data.mostRecentMoves = updateMostRecentMoves(data.mostRecentMoves, move);
   return data;
-};
+}
 
-export const updateMostRecentGames = (
+export function updateMostRecentGames(
   mostRecentGames: GameRecord[],
   board: Board
-) => {
+) {
   const gameRecord: GameRecord = {
     scores: {
       top: board.scores.top,
@@ -46,14 +46,14 @@ export const updateMostRecentGames = (
     turnsPlayed: board.turnsPlayed,
   };
 
-  mostRecentGames.push(gameRecord);
+  mostRecentGames.unshift(gameRecord);
   return mostRecentGames;
-};
+}
 
-export const updateAfterGame = (data: Data, board: Board) => {
+export function updateAfterGame(data: Data, board: Board) {
   const winner = board.currentTurn;
-  ++data.games.totalGames;
-  ++data.games.wins[winner];
+  ++data.totalGames;
+  ++data.wins[winner];
   data.mostRecentGames = updateMostRecentGames(data.mostRecentGames, board);
   return data;
-};
+}
--- src/write.ts
+++ src/write.ts
@@ -1,35 +1,45 @@
 import { Board, Data, GameRecord, MoveObject } from "./interfaces";
 const instruction = "Just push 'Submit new issue' without changing the title";
 
-export const createHTTPText = (text: string, plus = false) => {
-  return text
+export const createHTTPText = (text: string, plus = false) =>
+  text
     .replace(/ /g, plus ? "+" : "%20")
     .replace(/'/g, "%27")
     .replace(/\|/g, "%7C");
-};
 
-const createIssueLink = (title: string, body: string, innerText: string) => {
-  return `<a href="https://github.com/cbebe/chonka/issues/new?title=${title}&body=${body}">${innerText}</a>`;
-};
+const createIssueLink = (title: string, body: string, innerText: string) =>
+  `<a href="https://github.com/cbebe/chonka/issues/new?title=${title}&body=${body}">${innerText}</a>`;
 
-const createMoveLink = (pos: number, side: "top" | "bot", score: number) => {
+const createUserLink = (name: string) =>
+  `[@${name}](https://github.com/${name})`;
+
+const parseStats = (data: Data) => [
+  data.totalMoves,
+  Object.keys(data.players).length,
+  data.totalGames,
+];
+
+const createBadgeLink = (text: string, number: number, colour: string) =>
+  `![](https://img.shields.io/badge/${text}-${number}-${colour})`;
+
+function createMoveLink(pos: number, side: "top" | "bot", score: number) {
   const text = createHTTPText(
     `${instruction}. Please wait 30 seconds to check if you have an extra move or let someone else play the turn.`
   );
 
   return createIssueLink(`sungka%7C${side}%7C${pos}&`, text, String(score));
-};
+}
 
-const createNewGameLink = () => {
+function createNewGameLink() {
   const text = createHTTPText(`${instruction} to start a new game.`);
   return createIssueLink("sungka%7Cnew&", text, "new game");
-};
+}
 
-const createRow = (board: Board, side: "top" | "bot") => {
+function createRow(board: Board, side: "top" | "bot") {
   const isTurn = side === board.currentTurn;
   const reversed = side === "top";
   let holeArray = [...board[side]];
-  holeArray = reversed ? holeArray.reverse() : holeArray;
+  if (reversed) holeArray = holeArray.reverse();
 
   return holeArray
     .map(
@@ -38,51 +48,37 @@ const createRow = (board: Board, side: "top" | "bot") => {
           isTurn && score
             ? createMoveLink(reversed ? 6 - idx : idx, side, score)
             : score
-        }</td>\n`
+        }</td>`
     )
-    .join("");
-};
+    .join("\n");
+}
 
-const createTable = (board: Board) => {
-  let tableStr =
+function createTable(board: Board) {
+  const tableHead =
     "<table>\n<thead>\n<tr>\n<th>Top</th>\n" +
-    "<th colspan=7>Holes</th>\n<th>Bottom</th>\n</tr>\n</thead>\n<tbody>\n";
+    "<th colspan=7>Holes</th>\n<th>Bottom</th>\n</tr>\n</thead>\n<tbody>";
 
-  const topScore = `<tr>\n<td rowspan=2>${board.scores.top}</td>\n`;
+  const topScore = `<tr>\n<td rowspan=2>${board.scores.top}</td>`;
   const topRow = createRow(board, "top");
-  const botScore = `<td rowspan=2>${board.scores.bot}</td>\n</tr>\n<tr>\n`;
+  const botScore = `<td rowspan=2>${board.scores.bot}</td>\n</tr>\n<tr>`;
   const botRow = createRow(board, "bot");
 
-  tableStr +=
-    topScore + topRow + botScore + botRow + "</tr>\n<tbody>\n</table>\n";
+  const tableTail = "</tr>\n<tbody>\n</table>";
 
-  return tableStr;
-};
+  return [tableHead, topScore, topRow, botScore, botRow, tableTail].join("\n");
+}
 
-const createUserLink = (name: string) => {
-  return `[@${name}](https://github.com/${name})`;
-};
+function createRecentMoves(mostRecentMoves: MoveObject[]) {
+  if (mostRecentMoves.length === 0) return "";
 
-const createRecentMoves = (mostRecentMoves: MoveObject[]) => {
-  const heading = "**Most Recent Moves**\n\n";
-  let tableStr = "|Username|Side|Hole Index|\n|-|-|-|\n";
-  mostRecentMoves.forEach(({ name, side, idx }) => {
-    tableStr += `|${createUserLink(name)}|${side}|${idx}|\n`;
-  });
-  return heading + tableStr;
-};
+  const heading = "**Most Recent Moves**\n";
+  const tableHead = "|Username|Side|Hole Index|\n|-|-|-|";
+  const tableBody = mostRecentMoves.map(
+    ({ name, side, idx }) => `|${createUserLink(name)}|${side}|${idx}|`
+  );
 
-const parseStats = (data: Data) => {
-  const moves = data.games.totalMoves;
-  const players = Object.keys(data.players).length;
-  const games = data.games.totalGames;
-
-  return [moves, players, games];
-};
-
-const createBadgeLink = (text: string, number: number, colour: string) => {
-  return `![](https://img.shields.io/badge/${text}-${number}-${colour})`;
-};
+  return [heading, tableHead, ...tableBody].join("\n");
+}
 
 const createStatBadges = (data: Data) => {
   const stats = parseStats(data);
@@ -97,18 +93,18 @@ const createStatBadges = (data: Data) => {
     .join("\n");
 };
 
-export const createRecentGames = (mostRecentGames: GameRecord[]) => {
+export function createRecentGames(mostRecentGames: GameRecord[]) {
   const heading = "**Most Recent Games**\n";
   const head = "|Top Score|Bottom Score|Turns Played|\n|-|-|-|";
   const games = mostRecentGames.map(
     ({ scores, turnsPlayed }) => `|${scores.top}|${scores.bot}|${turnsPlayed}|`
   );
   return [heading, head, ...games].join("\n");
-};
+}
 
-export const createReadme = (board: Board, data: Data) => {
+export function createReadme(board: Board, data: Data) {
   const rulesLink =
-    " **Click on one of the holes** in the board to make a move. If you're not familiar with the game, click here for the [rules](https://mancala.fandom.com/wiki/Sungka#Rules).";
+    "**Click on one of the holes** in the board to make a move. If you're not familiar with the game, click here for the [rules](https://mancala.fandom.com/wiki/Sungka#Rules).";
 
   const turnString = board.gameOver
     ? `The game is over! :grin: Click here to start a ${createNewGameLink()}.`
@@ -133,4 +129,4 @@ export const createReadme = (board: Board, data: Data) => {
     createRecentMoves(data.mostRecentMoves),
     createRecentGames(data.mostRecentGames),
   ].join("\n\n");
-};
+}
