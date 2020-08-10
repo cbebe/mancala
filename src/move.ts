46c2fdca8afa9f1a141274f70be4a092033548e9
Charles Ancheta <cancheta@ualberta.ca>
2020-08-10 17:43:13 -0600

    :art: split makeMove for modularity

--- src/move.ts
+++ src/move.ts
@@ -5,27 +5,22 @@ function compareNumArrays(arr1: number[], arr2: number[]) {
   return arr1.every((num, idx) => num === arr2[idx]);
 }
 
-export function equalBoards(b1: Board, b2: Board) {
-  if (!compareNumArrays(b1.top, b2.top)) return false;
-  if (!compareNumArrays(b1.bot, b2.bot)) return false;
+export const equalBoards = (b1: Board, b2: Board) =>
+  !(
+    !compareNumArrays(b1.top, b2.top) ||
+    !compareNumArrays(b1.bot, b2.bot) ||
+    b1.currentTurn !== b2.currentTurn ||
+    b1.scores.top !== b2.scores.top ||
+    b1.scores.bot !== b2.scores.bot ||
+    b1.gameOver !== b2.gameOver ||
+    b1.turnsPlayed !== b2.turnsPlayed
+  );
 
-  if (b1.currentTurn !== b2.currentTurn) return false;
-  if (b1.scores.top !== b2.scores.top) return false;
-  if (b1.scores.bot !== b2.scores.bot) return false;
+const getOtherSide = (currentSide: Side) =>
+  currentSide === "top" ? "bot" : "top";
 
-  if (b1.gameOver !== b2.gameOver) return false;
-  if (b1.turnsPlayed === b2.turnsPlayed) return false;
-
-  return true;
-}
-
-function getOtherSide(currentSide: Side) {
-  return currentSide === "top" ? "bot" : "top";
-}
-
-function hasMoves(board: Board, side: Side) {
-  return board[side].some(hole => hole > 0);
-}
+const hasMoves = (board: Board, side: Side) =>
+  board[side].some(hole => hole > 0);
 
 const newBoard: Board = {
   currentTurn: "top",
@@ -39,26 +34,38 @@ const newBoard: Board = {
   turnsPlayed: 0,
 };
 
-export function newGame(board: Board): Board {
-  return board.gameOver ? newBoard : board;
-}
+export const newGame = (board: Board) => (board.gameOver ? newBoard : board);
 
 const getWinner = ({ top, bot }: Scores) =>
   top > bot ? "top" : bot > top ? "bot" : "draw";
 
+function evaluateAfterMove(board: Board, side: Side, extraMove: boolean) {
+  const otherSide = getOtherSide(side);
+  const playerHasMoves = hasMoves(board, side);
+  const enemyHasMoves = hasMoves(board, otherSide);
+
+  const moveAgain = (extraMove && playerHasMoves) || !enemyHasMoves;
+  board.currentTurn = moveAgain ? side : otherSide;
+
+  if (!moveAgain) ++board.turnsPlayed;
+
+  if (!(playerHasMoves || enemyHasMoves)) {
+    board.gameOver = true;
+    board.currentTurn = getWinner(board.scores);
+    ++board.turnsPlayed;
+  }
+
+  return board;
+}
+
 export function makeMove(board: Board, side: Side, holeIdx: number) {
+  const outOfRange = holeIdx < 0 || holeIdx > 6;
+  const invalidTurn = side !== board.currentTurn;
+
+  let shells = board[side][holeIdx];
+  if (!shells || outOfRange || invalidTurn || board.gameOver) return board;
+
   const newBoard = { ...board };
-  let shells = newBoard[side][holeIdx];
-
-  if (
-    !shells ||
-    holeIdx < 0 ||
-    holeIdx > 6 ||
-    side !== newBoard.currentTurn ||
-    newBoard.gameOver
-  )
-    return newBoard;
-
   const otherSide = getOtherSide(side);
   newBoard[side][holeIdx] = 0;
   let startingIdx = holeIdx + 1;
@@ -87,26 +94,12 @@ export function makeMove(board: Board, side: Side, holeIdx: number) {
 
     if (shells && currentSide === side) {
       ++newBoard.scores[side];
-      shells--;
-      if (shells === 0) {
-        extraMove = true;
-      }
+      --shells;
+      if (shells === 0) extraMove = true;
     }
     currentSide = getOtherSide(currentSide);
     startingIdx = 0;
   }
 
-  const playerHasMoves = hasMoves(board, side);
-  const enemyHasMoves = hasMoves(board, otherSide);
-
-  const moveAgain = (extraMove && playerHasMoves) || !enemyHasMoves;
-  newBoard.currentTurn = moveAgain ? side : otherSide;
-  if (!moveAgain) ++newBoard.turnsPlayed;
-
-  if (!(playerHasMoves || enemyHasMoves)) {
-    newBoard.gameOver = true;
-    newBoard.currentTurn = getWinner(newBoard.scores);
-  }
-
-  return newBoard;
+  return evaluateAfterMove(newBoard, side, extraMove);
 }
