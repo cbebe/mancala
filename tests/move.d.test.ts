d16a1eb22a7b1ce491d02f302e8dddbc4d25e242
Charles Ancheta <cancheta@ualberta.ca>
2020-08-10 17:42:02 -0600

    :art: create onePiece function to save space

--- tests/move.d.test.ts
+++ tests/move.d.test.ts
@@ -6,8 +6,8 @@ const board = (
   bot: number[],
   currentTurn: Side | "draw",
   scores: [number, number] = [0, 0],
-  gameOver = false,
-  turnsPlayed = 0
+  turnsPlayed = 0,
+  gameOver = false
 ): Board => {
   return {
     currentTurn,
@@ -22,24 +22,37 @@ const board = (
 const newArr = () => [7, 7, 7, 7, 7, 7, 7];
 const empty = () => [0, 0, 0, 0, 0, 0, 0];
 
+// when there's only one piece on the row
+const onePiece = (i: number) => {
+  const arr = empty();
+  arr[i] = 1;
+  return arr;
+};
+
 test("Avalanche mechanic on own side", () => {
   const bot = [1, 0, 1, 1, 1, 1, 1];
   const i = board([1, 3, 1, 1, 1, 0, 1], bot, "top");
-  const e = board([0, 0, 2, 2, 2, 1, 1], bot, "bot");
+  const e = board([0, 0, 2, 2, 2, 1, 1], bot, "bot", [0, 0], 1);
   const o = makeMove(i, "top", 0);
   expect(equalBoards(o, e)).toBe(true);
 });
 
 test("Avalance mechanic on both sides", () => {
   const i = board([0, 0, 0, 5, 0, 0, 0], [6, 0, 0, 0, 0, 0, 0], "top");
-  const e = board([0, 0, 0, 0, 1, 1, 1], [0, 1, 1, 1, 1, 1, 0], "bot", [3, 0]);
+  const e = board(
+    [0, 0, 0, 0, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1, 0],
+    "bot",
+    [3, 0],
+    1
+  );
   const o = makeMove(i, "top", 3);
   expect(equalBoards(o, e)).toBe(true);
 });
 
 test("Capture mechanic", () => {
   const i = board([0, 0, 0, 0, 0, 0, 9], [0, 0, 0, 0, 0, 0, 10], "top");
-  const e = board([0, 0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1, 0], "bot", [13, 0]);
+  const e = board(empty(), [1, 1, 1, 1, 1, 1, 0], "bot", [13, 0], 1);
   const o = makeMove(i, "top", 6);
   expect(equalBoards(o, e)).toBe(true);
 });
@@ -52,28 +65,28 @@ test("Extra move", () => {
 });
 
 test("No moves even when the current player has extra move", () => {
-  const i = board([0, 0, 0, 0, 0, 0, 1], newArr(), "top");
-  const e = board(empty(), newArr(), "bot", [1, 0]);
+  const i = board(onePiece(6), newArr(), "top");
+  const e = board(empty(), newArr(), "bot", [1, 0], 1);
   const o = makeMove(i, "top", 6);
   expect(equalBoards(o, e)).toBe(true);
 });
 
 test("No moves for the other player", () => {
-  const i = board(empty(), [0, 0, 0, 0, 0, 1, 0], "bot");
-  const e = board(empty(), [0, 0, 0, 0, 0, 0, 1], "bot");
+  const i = board(empty(), onePiece(5), "bot");
+  const e = board(empty(), onePiece(6), "bot");
   const o = makeMove(i, "bot", 5);
   expect(equalBoards(o, e)).toBe(true);
 });
 
 test("Game over", () => {
-  const i = board(empty(), [0, 0, 0, 0, 0, 0, 1], "bot");
-  const e = board(empty(), empty(), "bot", [0, 1], true);
+  const i = board(empty(), onePiece(6), "bot");
+  const e = board(empty(), empty(), "bot", [0, 1], 1, true);
   const o = makeMove(i, "bot", 6);
   expect(equalBoards(o, e)).toBe(true);
 });
 
 test("Creates new game", () => {
-  const i = board(empty(), empty(), "draw", [49, 49], true);
+  const i = board(empty(), empty(), "draw", [49, 49], 1, true);
   const e = board(newArr(), newArr(), "top");
   const o = newGame(i);
   expect(equalBoards(o, e)).toBe(true);
@@ -87,22 +100,15 @@ test("Cancel attempt to create new game", () => {
 });
 
 test("Getting a draw", () => {
-  const i = board(empty(), [0, 0, 0, 0, 0, 0, 1], "bot", [1, 0]);
-  const e = board(empty(), empty(), "draw", [1, 1], true);
+  const i = board(empty(), onePiece(6), "bot", [1, 0]);
+  const e = board(empty(), empty(), "draw", [1, 1], 1, true);
   const o = makeMove(i, "bot", 6);
   expect(equalBoards(o, e)).toBe(true);
 });
 
 test("Increment turns played", () => {
-  const i = board([0, 1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0], "top");
-  const e = board(
-    [0, 0, 1, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0],
-    "bot",
-    [0, 0],
-    false,
-    1
-  );
+  const i = board(onePiece(1), onePiece(1), "top");
+  const e = board(onePiece(2), onePiece(1), "bot", [0, 0], 1);
   const o = makeMove(i, "top", 1);
   expect(equalBoards(o, e)).toBe(true);
 });
